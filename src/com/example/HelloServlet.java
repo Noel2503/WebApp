d5b131c7826c03e8b702ca96807d55182a164d40
Noel Yesuraj.A <137898788+Noel2503@users.noreply.github.com>
2024-11-08 14:36:37 +0530

    Create HelloServlet.java

---
+++ src/com/example/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>Hello, World from Servlet!</h1></body></html>");
+    }
+}
